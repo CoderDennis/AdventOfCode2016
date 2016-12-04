471a9f88627691ac04543f6f66d85edb20d00767
CoderDennis <dodava@gmail.com>
2016-12-04 00:47:46 -0600

    day 3 part 2

--- day03/day03.ex
+++ day03/day03.ex
@@ -7,6 +7,33 @@ defmodule Day03 do
     |> Enum.count
   end
 
+  def count_valid_triangles_by_columns() do
+    File.stream!("input.txt")
+    |> Stream.map(fn s -> String.split(s, " ") |> Enum.reject(&(&1 == "")) end)
+    |> read_triangles_by_columns
+    |> Enum.filter(&valid_triangle?/1)
+    |> Enum.count
+  end
+
+  @doc ~s"""
+  # Examples
+
+    iex> Day03.read_triangles_by_columns([["101", "301", "501"], ["102", "302", "502"], ["103", "303", "503"], ["201", "401", "601"], ["202", "402", "602"], ["203", "403", "603"]])
+    [["101", "102", "103"],["201", "202", "203"],["301", "302", "303"],["401", "402", "403"],["501", "502", "503"],["601", "602", "603"]]
+
+  """
+  def read_triangles_by_columns(rows) do
+    rows
+    |> transpose
+    |> List.flatten
+    |> Enum.chunk(3)
+  end
+
+  defp transpose([[]|_]), do: []
+  defp transpose(a) do
+    [Enum.map(a, &hd/1) | transpose(Enum.map(a, &tl/1))]
+  end
+
   @doc ~s"""
   # Examples
 
