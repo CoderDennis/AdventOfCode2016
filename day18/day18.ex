ef047107865081d2b1367f369bf15610557b26e2
Dennis Palmer <dodava@gmail.com>
2016-12-17 23:45:13 -0600

    day 18 part 1

---
+++ day18/day18.ex
@@ -0,0 +1,43 @@
+defmodule Day18 do
+
+  def puzzle_answer() do
+    ".^^^.^.^^^^^..^^^..^..^..^^..^.^.^.^^.^^....^.^...^.^^.^^.^^..^^..^.^..^^^.^^...^...^^....^^.^^^^^^^"
+    |> count_safe_tiles_in_rows(40)
+  end
+
+  @doc """
+  iex> Day18.count_safe_tiles_in_rows(".^^.^.^^^^", 10)
+  38
+  """
+  def count_safe_tiles_in_rows(input, rows) do
+    input
+    |> String.codepoints
+    |> Stream.iterate(&next_row/1)
+    |> Enum.take(rows)
+    |> Enum.reduce(0, &count_safe_tiles/2)
+  end
+
+  def count_safe_tiles(row, total) do
+    Enum.count(row, &(&1 == ".")) + total
+  end
+
+  @doc """
+  iex> Day18.next_row("..^^." |> String.codepoints) |> List.to_string
+  ".^^^^"
+
+  iex> Day18.next_row(".^^^^" |> String.codepoints) |> List.to_string
+  "^^..^"
+  """
+  def next_row(row) do
+    ["." | row]
+    |> Enum.chunk(3, 1, ["."])
+    |> Enum.map(&new_tile/1)
+  end
+
+  def new_tile(["^", "^", "."]), do: "^"
+  def new_tile([".", "^", "^"]), do: "^"
+  def new_tile(["^", ".", "."]), do: "^"
+  def new_tile([".", ".", "^"]), do: "^"
+  def new_tile(_), do: "."
+
+end
