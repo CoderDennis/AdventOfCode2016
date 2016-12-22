1d1de17296181db1d6868d1e18a13f478a84189a
Dennis Palmer <dodava@gmail.com>
2016-12-22 00:09:19 -0600

    moves parse_node into GridNode module

--- day22/day22.ex
+++ day22/day22.ex
@@ -1,13 +1,9 @@
-defmodule Node do
-  defstruct [:name, :size, :used, :avail]
-end
-
 defmodule Day22 do
 
   def count_viable_pairs() do
     File.stream!("input.txt")
     |> Stream.drop(2)
-    |> Enum.map(&parse_node/1)
+    |> Enum.map(&GridNode.parse_node/1)
     |> count_viable_pairs
     |> MapSet.new
     |> MapSet.size
@@ -22,11 +18,15 @@ defmodule Day22 do
         do: [a.name, b.name] |> Enum.sort |> List.to_tuple
   end
 
+end
+
+defmodule GridNode do
+  defstruct [:name, :size, :used, :avail]
+
   def parse_node("/dev/grid/node-" <> <<name::bytes-size(8)>> <> <<size::bytes-size(4)>> <> "T " <> <<used::bytes-size(4)>> <> "T  " <> <<avail::bytes-size(4)>> <> _) do
-    %Node{name:  name  |> String.trim,
+    %GridNode{name:  name  |> String.trim,
           size:  size  |> String.trim |> String.to_integer,
           used:  used  |> String.trim |> String.to_integer,
           avail: avail |> String.trim |> String.to_integer}
   end
-
 end
