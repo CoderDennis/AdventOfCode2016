31d540dd709231dc42922894c9c248927ca95fb5
Dennis Palmer <dodava@gmail.com>
2016-12-26 16:47:38 -0600

    day 20 part 2

--- day20/day20.ex
+++ day20/day20.ex
@@ -5,15 +5,30 @@ defmodule Day20 do
            "4-7"]
 
   # for line <- @sample do
+  # for line <- File.stream!("input.txt") do
+  #   [_, min, max] = Regex.run(~r/(\d+)-(\d+)/, line)
+  # end
+
   for line <- File.stream!("input.txt") do
-    [_, min, max] = Regex.run(~r/(\d+)-(\d+)/, line)
-    def blocked?(ip) when ip >= unquote(min |> String.to_integer) and ip <= unquote(max |> String.to_integer), do: true
+    [_, min_str, max_str] = Regex.run(~r/(\d+)-(\d+)/, line)
+    min = min_str |> String.to_integer
+    max = max_str |> String.to_integer
+    def blocked?(ip) when ip >= unquote(min) and ip <= unquote(max), do: true
+    def skip_blocked(ip) when ip >= unquote(min) and ip < unquote(max), do: skip_blocked(unquote(max))
   end
   def blocked?(_), do: false
+  def skip_blocked(ip), do: ip + 1
 
-  def first_unblocked() do
-    Stream.iterate(0, &(&1+1))
+  def first_unblocked(start \\ 0) do
+    Stream.iterate(start, &(&1+1))
     |> Enum.find(&(not blocked?(&1)))
+  end
+
+  def count_unblocked() do
+    Stream.iterate(0, &skip_blocked/1)
+    |> Stream.filter(&(not blocked?(&1)))
+    |> Stream.take_while(&(&1 <= 4294967295))
+    |> Enum.count
   end
 
 end
