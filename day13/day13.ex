22e06d6d24f86f0c90ecf09789b92d0c772f0611
Dennis Palmer <dodava@gmail.com>
2016-12-25 22:33:12 -0600

    day 13 part 1

--- day13/day13.ex
+++ day13/day13.ex
@@ -1,4 +1,3 @@
-
 defmodule Day13 do
   require Integer
 
@@ -10,15 +9,17 @@ defmodule Day13 do
     draw_maze(40, 50, 31, 39, 1352)
   end
 
-  @doc ~s"""
-
-  """
   def navigate_sample() do
     navigate({7, 4}, 10)
   end
 
-  def navigate({goal_x, goal_y}, favorite_number) do
+  def navigate_puzzle() do
+    navigate({31, 39}, 1352)
+  end
 
+  def navigate(goal, favorite_number) do
+    AnswerCollector.start_link
+    Pathfinder.start({1, 1}, goal, favorite_number, MapSet.new)
   end
 
   @doc ~s"""
@@ -28,6 +29,7 @@ defmodule Day13 do
   iex> Day13.wall_or_open_space(1, 1, 10)
   :open_space
   """
+  def wall_or_open_space(x, y, _) when x < 0 or y < 0, do: :invalid
   def wall_or_open_space(x, y, favorite_number) do
     (x*x + 3*x + 2*x*y + y + y*y + favorite_number)
     |> Integer.to_charlist(2)
@@ -66,5 +68,81 @@ defmodule Day13 do
 
   defp display(:wall), do: "#"
   defp display(:open_space), do: "."
+
+end
+
+defmodule Pathfinder do
+  use GenServer
+
+  def start(position, goal, number, path) do
+    {:ok, pid} = GenServer.start(__MODULE__, {position, goal, number, path})
+    GenServer.cast(pid, :go)
+    {:ok, pid}
+  end
+
+  def handle_cast(:go, {goal, goal, _number, path} = state) do
+    AnswerCollector.save_answer(MapSet.size(path))
+    {:stop, :normal, state}
+  end
+  def handle_cast(:go, {position, goal, number, path} = state) do
+    position
+    |> possible_steps(number, path)
+    |> Enum.each(&(Pathfinder.start(&1, goal, number, MapSet.put(path, &1))))
+    {:stop, :normal, state}
+  end
+
+  def possible_steps({x, y}, number, path) do
+    [{x + 1, y},
+     {x - 1, y},
+     {x, y + 1},
+     {x, y - 1}]
+    |> Enum.filter(&(space_not_in_path?(&1, number, path)))
+  end
+
+  def space_not_in_path?({x, y} = position, number, path) do
+    if MapSet.member?(path, position) do
+      false
+    else
+      Day13.wall_or_open_space(x, y, number) == :open_space
+    end
+  end
+
+end
+
+defmodule AnswerCollector do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, 0, name: :answer)
+  end
+
+  def save_answer(length) do
+    GenServer.cast(:answer, length)
+  end
+
+  def get() do
+    GenServer.call(:answer, :get)
+  end
+
+  def reset() do
+    GenServer.call(:answer, :reset)
+  end
+
+  def handle_cast(length, 0) do
+    {:noreply, length}
+  end
+  def handle_cast(length, shortest) when length < shortest do
+    {:noreply, length}
+  end
+  def handle_cast(_, state) do
+    {:noreply, state}
+  end
+
+  def handle_call(:get, _from, length) do
+    {:reply, length, length}
+  end
+  def handle_call(:reset, _from, _) do
+    {:reply, :ok, 0}
+  end
 
 end
